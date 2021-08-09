41b506d8faa9ce9f272a15ca674a71cc71c96f7b
Izotov <izotda@gmail.com>
2021-08-10 01:25:43 +0700

    added view for coin information

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Coins from '../views/Coins.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,7 @@ const routes = [
     name: 'Home',
     component: Home
   },
+
   {
     path: '/about',
     name: 'About',
@@ -17,6 +19,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+
+  {
+    path: '/coins/:coin',
+    name: 'Coins',
+    component: Coins,
+    props: true
   }
 ]
 
---
+++ src/views/Coins.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <img :src="coinData.imageUrl" alt="Token icon">
+    <h1>{{ coinData.fullName }}</h1>
+    <p class="coin-description">{{ coinData.description }}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    coin: {
+      type: String,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      coinData: null
+    }
+  },
+
+  created() {
+    const coinList = JSON.parse(localStorage.getItem('coin-list'));
+    this.coinData = coinList[this.coin.toUpperCase()];
+  }
+}
+</script>
+
+<style scoped>
+.coin-description {
+  padding: 0 20px;
+  text-align: justify;
+}
+</style>  
